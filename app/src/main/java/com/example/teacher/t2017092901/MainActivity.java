f9cf4c88de15a6bcce7d4184abc06ca5c277ea70
Shaojung Lu <shaojung@gmail.com>
2017-09-29 09:21:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/teacher/t2017092901/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.teacher.t2017092901;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView wv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wv = (WebView) findViewById(R.id.webView);
+        wv.loadUrl("http://www.google.com.tw");
+    }
+}
